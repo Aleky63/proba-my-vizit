262e905ed425baad3b583d0c4297e17c9166e844
Aleksey <134486666+Aleky63@users.noreply.github.com>
2025-03-29 12:19:05 +0300

    01_29.03

--- js/script.js
+++ js/script.js
@@ -3,22 +3,22 @@ let center = [46.672205, 38.267018];
 function init() {
   let map = new ymaps.Map("map-test", {
     center: center,
-    zoom: 10,
+    zoom: 8,
   });
 
   // let placemark = new ymaps.Placemark(
-    // center,
-    // {
-      // balloonContentHeader: "Хедер балуна",
-      // balloonContentBody: ",Боди балуна",
-      // balloonContentFooter: ",Подвал",
-    // },
-    // {
-      // iconLayout: "default#image",
-      // iconImageHref: "https://cdn-icons-png.flaticon.com/256/2776/2776067.png",
-      // iconImageSize: [50, 50],
-      // iconImageOffset: [0, 0],
-    // }
+  // center,
+  // {
+  // balloonContentHeader: "Хедер балуна",
+  // balloonContentBody: ",Боди балуна",
+  // balloonContentFooter: ",Подвал",
+  // },
+  // {
+  // iconLayout: "default#image",
+  // iconImageHref: "https://cdn-icons-png.flaticon.com/256/2776/2776067.png",
+  // iconImageSize: [50, 50],
+  // iconImageOffset: [0, 0],
+  // }
   // );
 
   let placemark = new ymaps.Placemark(
@@ -49,7 +49,6 @@ function init() {
   // map.controls.remove("zoomControl"); // удаляем контрол зуммирования
   // map.controls.remove("rulerControl"); // удаляем контрол правил
   // map.behaviors.disable(["scrollZoom"]); // отключаем скролл карты (опционально)
-
 
   map.geoObjects.add(placemark);
   placemark.balloon.open();
